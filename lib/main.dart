ce6a4c2532f4034b1b31c145dbd389e23b95da6e
Deep27 <geep2795@gmail.com>
2020-01-03 22:16:30 +0300

    Refreshing course.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'package:cash_expenses/transaction.dart';
 
 void main() => runApp(CashExpensesApp());
 
 class CashExpensesApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Old friends',
+        amount: double.infinity,
+        date: DateTime.now()),
+    Transaction(
+        id: 't3',
+        title: 'New thoughts',
+        amount: double.infinity,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Personal Expenses',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          accentColor: Colors.amber,
-          fontFamily: 'Quicksand',
-          appBarTheme: AppBarTheme(
-            textTheme: ThemeData.light().textTheme.copyWith(
-                  title: TextStyle(
-                    fontFamily: 'OpenSans',
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
         ),
-        home: HomePage());
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                ...transactions
+                    .map((t) => Card(child: Text('${t.amount} + ${t.title}')))
+                    .toList(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
