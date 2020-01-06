d4e56e46b7b0e0254811577ef2b7bea449eed081
Deep27 <geep2795@gmail.com>
2020-01-06 22:46:55 +0300

    Split better.

--- lib/widgets/transaction/transaction_list_widget.dart
+++ lib/widgets/transaction/transaction_list_widget.dart
@@ -4,24 +4,10 @@ import 'package:cash_expenses/models/transaction/transaction.dart';
 import 'package:cash_expenses/widgets/transaction/transaction_widget.dart';
 
 class TransactionListWidget extends StatelessWidget {
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-        id: 't2',
-        title: 'Old friends',
-        amount: double.infinity,
-        date: DateTime.now()),
-    Transaction(
-        id: 't3',
-        title: 'New thoughts',
-        amount: double.infinity,
-        date: DateTime.now()),
-  ];
+
+  final List<Transaction> _transactions;
+
+  TransactionListWidget(this._transactions);
 
   @override
   Widget build(BuildContext context) {
--- lib/widgets/transaction/user_transactions_widget.dart
+++ lib/widgets/transaction/user_transactions_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:cash_expenses/models/transaction/transaction.dart';
 import 'package:cash_expenses/widgets/transaction/new_transaction_input_widget.dart';
 import 'package:cash_expenses/widgets/transaction/transaction_list_widget.dart';
 
@@ -9,12 +10,32 @@ class UserTransactionsWidget extends StatefulWidget {
 }
 
 class _UserTransactionsWidgetState extends State<UserTransactionsWidget> {
+
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+        id: 't2',
+        title: 'Old friends',
+        amount: double.infinity,
+        date: DateTime.now()),
+    Transaction(
+        id: 't3',
+        title: 'New thoughts',
+        amount: double.infinity,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         NewTransactionInputWidget(),
-        TransactionListWidget(),
+        TransactionListWidget(_transactions),
       ],
     );
   }
