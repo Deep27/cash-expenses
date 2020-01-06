2c18223f8ef9a188a09ad3b2e83393a6272d8754
Deep27 <geep2795@gmail.com>
2020-01-06 22:38:56 +0300

    More splitting into widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:cash_expenses/widgets/transaction/transaction_list_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'package:cash_expenses/widgets/chart_widget.dart';
+import 'package:cash_expenses/widgets/transaction/new_transaction_input_widget.dart';
+import 'package:cash_expenses/widgets/transaction/transaction_list_widget.dart';
 
 void main() => runApp(CashExpensesApp());
 
 class CashExpensesApp extends StatelessWidget {
-  final _titleController = TextEditingController();
-  final _amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,39 +17,8 @@ class CashExpensesApp extends StatelessWidget {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: _titleController,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: _amountController,
-                    ),
-                    FlatButton(
-                      child: Text('Add transaction'),
-                      textColor: Colors.purple,
-                      onPressed: () => print(
-                          '${_titleController.text} & ${_amountController.text}'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            ChartWidget(),
+            NewTransactionInputWidget(),
             TransactionListWidget(),
           ],
         ),
